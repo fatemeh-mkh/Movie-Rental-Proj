291a4c7f6403dcb9286811d350ec91ccc8fa3158
Khadamat <fatem.mokhtari@yahoo.com>
2024-05-09 21:33:41 +0330

    calling manager and customer login functions in Main class

---
+++ Main.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        customer c1= new customer();
+        c1.login();
+        manager mn= new manager();
+        mn.manager_login();
+
+
+    }
+}
